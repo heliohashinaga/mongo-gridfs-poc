ec262c496e47d25d82ae02b54ea513b6e726943e
Helio Higuchi Hashinaga <helio.hashinaga@clear.sale>
2025-11-06 10:05:57 -0300

    init project

---
+++ MongoGridFS/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Extensions.DiagnosticSources;
+using MongoDB.Driver.GridFS;
+
+Console.WriteLine("MongoGridFS started");
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
+        .SetBasePath(builder.Environment.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+#if DEBUG
+builder.Configuration.AddUserSecrets(typeof(Program).Assembly, true);
+#endif
+
+var connectionString = builder.Configuration.GetConnectionString("MongoDB");
+var database = builder.Configuration.GetValue<string>("MongoDatabase");
+
+var settings = MongoClientSettings.FromConnectionString(connectionString);
+var options = new InstrumentationOptions { CaptureCommandText = true };
+settings.ClusterConfigurator = cc => cc.Subscribe(new DiagnosticsActivityEventSubscriber(options));
+var mongoClient = new MongoClient(settings);
+var mongoDatabase = mongoClient.GetDatabase(database);
+var gridFS = new GridFSBucket(mongoDatabase);
+
+var filePath = "test_file.txt";
+
+ObjectId id;
+
+using (var stream = File.OpenRead(filePath))
+{
+        id = await gridFS.UploadFromStreamAsync(Path.GetFileName(filePath), stream);
+        Console.WriteLine($"File uploaded with id: {id}");
+}
+
+await gridFS.DownloadToStreamAsync(id, File.OpenWrite("downloaded_test_file.txt"));
+
+Console.WriteLine("MongoGridFS finished");
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # mongo-gridfs-poc
+Example how to use MongoDB GridFS with .NET to upload and download files.
+
+## Configuration
+
+### appsettings.json
+```json
+{
+  "ConnectionStrings": {
+    "MongoDB": "mongodb://localhost:27017"
+  },
+  "MongoDatabase": "yourDatabaseName"
+}
+```
+
+### User Secrets (optional)
+In DEBUG mode, you can use User Secrets to store sensitive credentials.
+
+```shell
+dotnet user-secrets init
+dotnet user-secrets set "ConnectionStrings:MongoDB" "mongodb://username:password@host:port"
+```
+
+## How to Use
+1. Place the file you want to upload in the project directory (e.g., test_file.txt)
+2. Run the project:
+    ```shell
+    dotnet run
+    ```
+3. The file will be uploaded to MongoDB and downloaded as downloaded_test_file.txt
